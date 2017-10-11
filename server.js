f4dfa787fccf316dddaaf14decbe0918df51818a
graham-willis-druid <graham.willis.ea@gmail.com>
2017-10-11 13:55:33 +0100

    Changes to the configuration

--- server.js
+++ server.js
@@ -28,94 +28,95 @@ server.connection({
     port: process.env.PORT
 });
 
-// Register the logging plugin
-server.register({
-    register: require('good'),
-    options: {
-        reporters: {
-            winston: [ system.goodWinstonStream ]
-        }
-    }
-}, (err) => {
-    if (err) {
+// A function to provision and start the Hapi server
+(async () => {
+    try {
+
+        // Register the logging plugin
+        await server.register({
+            register: require('good'),
+            options: {
+                reporters: {
+                    winston: [system.goodWinstonStream]
+                }
+            }
+        });
+
+        await server.register({
+            register: require('inert')
+        });
+
+        // Register the server methods
+        server.method([{
+            name: 'sessionData',
+            // the 'next' callback must be used to return values
+            method: function (sid, data, next) {
+                next(null, 'Welcome: ' + data);
+            },
+            options: {
+                cache: {
+                    segment: 'session-data',
+                    expiresIn: 60000,
+                    staleIn: 30000,
+                    staleTimeout: 10000,
+                    generateTimeout: 100
+                },
+                generateKey: function (sid) {
+                    return sid + '_sessionData';
+                }
+            }
+        }]);
+
+        // Register Hapi Authorization cookies
+        await server.register({
+            register: require('hapi-auth-cookie')
+        });
+
+        // Create a Hapi-server cache policy
+        // To hold the authenticated user data. This will
+        // live in the plug-in cache
+        const cache = server.cache({
+            segment: 'authenticated-sessions',
+            expiresIn: srvcfg.cache.authorization.timeToLive
+        });
+
+        server.app.cache = cache;
+
+        server.auth.strategy('session', 'cookie', true, {
+            password: srvcfg.authorization.cookie.ironCookiePassword,
+            cookie: 'sid',
+            redirectTo: '/login',
+            isSecure: false,
+            // An optional validation function used to validate the content
+            // of the session cookie on each request
+            validateFunc: function (request, session, callback) {
+                server.app.cache.get(session.sid, (err, cached) => {
+
+                    if (err) {
+                        return callback(err, false);
+                    }
+
+                    if (!cached) {
+                        return callback(null, false);
+                    }
+
+                    // If we are validated add the sid to the request object
+                    server.app.sid = session.sid;
+
+                    // Return a validated callback function
+                    return callback(null, true, cached.account);
+                });
+            }
+        });
+
+        // Set up the routing
+        server.route(require('./app/routes'));
+
+        // Start the server
+        await server.start();
+        system.logger.info(`Server started at ${server.info.uri}`);
+    } catch (err) {
         system.logger.log('error', err);
         process.exit(1);
     }
-});
-
-// Register server methods
-server.method([{
-    name: 'sessionData',
-    // the 'next' callback must be used to return values
-    method: function (sid, data, next) {
-        next(null, 'Welcome: ' + data);
-    },
-    options: {
-        cache: {
-            segment: 'session-data',
-            expiresIn: 60000,
-            staleIn: 30000,
-            staleTimeout: 10000,
-            generateTimeout: 100
-        },
-        generateKey: function (sid) {
-            return sid + '_sessionData';
-        }
-    }
-}]);
-
-// Register Hapi Authorization cookies
-server.register({
-    register: require('hapi-auth-cookie')
-}, (err) => {
-
-    if (err) {
-        system.logger.log('error', err);
-        process.exit(1);
-    }
-
-    // Create a Hapi-server cache policy
-    // To hold the authenticated user data. This will
-    // live in the plug-in cache
-    const cache = server.cache({
-        segment: 'authenticated-sessions',
-        expiresIn: srvcfg.session.timeToLive
-    });
-
-    server.app.cache = cache;
-
-    server.auth.strategy('session', 'cookie', true, {
-        password: srvcfg.session.ironCookiePassword,
-        cookie: 'sid',
-        redirectTo: '/login',
-        isSecure: false,
-        // An optional validation function used to validate the content
-        // of the session cookie on each request
-        validateFunc: function (request, session, callback) {
-            server.app.cache.get(session.sid, (err, cached) => {
-
-                if (err) {
-                    return callback(err, false);
-                }
-
-                if (!cached) {
-                    return callback(null, false);
-                }
-
-                // If we are validated add the sid to the request object
-                server.app.sid = session.sid;
-
-                // Return a validated callback function
-                return callback(null, true, cached.account);
-            });
-        }
-    });
-
-    // Set up the routing
-    server.route(require('./app/routes'));
-});
-
-// Start the server
-server.start(() => {
-    system.logger.info(`Server started at ${server.info.uri}`);
-});
+})();
