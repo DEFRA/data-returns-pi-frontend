fdda339e2d91b8cd6a2ef1f4918d6d15fc6ad2d4
graham-willis-druid <graham.willis.ea@gmail.com>
2017-10-20 12:40:56 +0100

    Added file logging and included project .css. Separated logger from system packages

---
+++ src/lib/logging.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * System wide resources
+ * @type {*|yaml}
+ */
+const winston = require('winston');
+const GoodWinston = require('good-winston');
+const system = require('./system');
+const _ = require('lodash');
+
+const commonLoggingOptions = {
+    'level': process.env.NODE_ENV === 'local' ? system.configuration.logging.level : 'info',
+    'colorize': true,
+    'silent': false,
+    'timestamp': true,
+    'json': false,
+    'showLevel': true,
+    'handleExceptions': true,
+    'humanReadableUnhandledException': true
+};
+
+const fileLoggingOptions = {
+    'filename': 'logs/datareturns.log',
+    'maxsize': 2 * Math.pow(2, 20),
+    'maxFiles': 10,
+    'tailable': true
+};
+
+const winlogger = new winston.Logger({
+    transports: [
+        new (winston.transports.Console)(commonLoggingOptions),
+        new (winston.transports.File)(_.merge({}, commonLoggingOptions, fileLoggingOptions))
+    ]
+});
+
+module.exports = {
+    /**
+   * Return the system logger
+   */
+    logger: winlogger,
+
+    /**
+   * Return the winston-good interface for Hapi
+   */
+    goodWinstonStream: new GoodWinston({ winston: winlogger })
+};
